8e0ae86ddea9e884ab42584d296996ed6f41caed
j0eTheRipper <j0eTheRipper0010@gmail.com>
2021-12-29 16:19:08 +0300

    restore feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from os.path import abspath, dirname, join
+from datetime import date
 
 
 app = Flask(__name__)
@@ -10,23 +11,112 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Shelf(db.Model):
-    __tablename__ = 'shelf'
+class BookExists(BaseException):
+    pass
 
-    id = db.Column(db.String, primary_key=True)
-    subject = db.Column(db.String)
-    books = db.relationship('Book', back_populates='shelf')
+
+class BookNotFound(BaseException):
+    pass
+
+
+class OutOfBooks(BaseException):
+    pass
+
+
+class BorrowNotFound(BaseException):
+    pass
 
 
 class Book(db.Model):
     __tablename__ = 'book'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, index=True)
+    title = db.Column(db.String(32), unique=True, index=True)
     count = db.Column(db.Integer, default=1)
-    shelf_id = db.Column(db.String, db.ForeignKey('shelf.id'))
+    borrowed = db.relationship('Borrows', backref='book')
 
-    shelf = db.relationship('Shelf', back_populates='books')
+
+class Borrows(db.Model):
+    __tablename__ = 'borrows'
+
+    id = db.Column(db.Integer, primary_key=True)
+    borrower = db.Column(db.String(32))
+    book_title = db.Column(db.Integer, db.ForeignKey('book.title'))
+    borrow_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
+
+
+def add_book(title: str, count=1):
+    title = ' '.join(title.split()).title()
+    book_exists = Book.query.filter_by(title=title).first()
+
+    if not book_exists:
+        book = Book(title=title, count=count)
+
+        db.session.add(book)
+        db.session.commit()
+    else:
+        raise BookExists
+
+
+def borrow_book(book: str, name: str, borrow_date: date, return_date: date):
+    book = ' '.join(book.split()).title()
+    name = ' '.join(name.split()).title()
+    book = Book.query.filter_by(title=book).first()
+
+    if book and book.count:
+        borrow = Borrows(
+            borrower=name,
+            book=book,
+            borrow_date=borrow_date,
+            return_date=return_date,
+        )
+
+        book.count -= 1
+
+        db.session.add_all([borrow, book])
+        db.session.commit()
+    elif not book:
+        raise BookNotFound
+    elif not book.count:
+        raise OutOfBooks
+
+
+class Return:
+    def __init__(self, borrow_id: int = 0, student_name: str = ''):
+        self.student_name = ' '.join(student_name.split()).title() if student_name else ''
+        self.borrow_id = borrow_id
+
+        self.return_book()
+
+    def return_book(self):
+        if self.borrow_id:
+            self.return_by_id()
+        elif self.student_name:
+            self.return_by_student_name()
+
+    def return_by_student_name(self):
+        borrow = Borrows.query.filter_by(borrower=self.student_name).first()
+        if borrow:
+            self.return_(borrow)
+        else:
+            raise BorrowNotFound
+
+    def return_by_id(self):
+        borrow = Borrows.query.filter_by(id=self.borrow_id).first()
+
+        if borrow:
+            self.return_(borrow)
+        else:
+            raise BorrowNotFound
+
+    @staticmethod
+    def return_(borrow):
+        book = borrow.book
+        book.count += 1
+        db.session.add(book)
+        db.session.delete(borrow)
+        db.session.commit()
 
 
 @app.route('/')
@@ -56,4 +146,30 @@ def borrow_get():
 
 @app.route('/search')
 def book_search():
-    return render_template('student/look_up.html')
+    user_query = request.args.get('search')
+
+    context = {
+        'book_title': '',
+        'book_shelf': '',
+    }
+
+    if user_query:
+        user_query = ' '.join(user_query.lower().split())
+        result = Book.query.filter_by(title=user_query).first()
+        if result:
+            context['book_title'] = result.title
+            context['book_shelf'] = result.shelf_id
+
+    return render_template('student/look_up.html', **context)
+
+
+@app.shell_context_processor
+def imports():
+    return dict(
+        db=db,
+        add_book=add_book,
+        borrow_book=borrow_book,
+        Book=Book,
+        Borrows=Borrows,
+        Return=Return,
+    )
