49654491880d148b3bda2b878c960a096e2024f8
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-01-21 21:03:22 +0300

    centered a div

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_bootstrap import Bootstrap
 from os import makedirs
 from os.path import isdir, join
 
@@ -18,9 +19,12 @@ def create_app(test_config=None):
     if not isdir(app.instance_path):
         makedirs(app.instance_path)
 
+    Bootstrap(app)
+
     @app.route('/')
     def _(): 
-        return render_template('home.html')
+        is_logged_in = bool(session.get('user'))
+        return render_template('home.html', is_logged_in=is_logged_in)
     
 
     with app.app_context():
