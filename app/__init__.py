5687ef3b2ed5f7f269c2b5b13fae83040761a71a
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-02-03 00:43:00 +0300

    Added home page a blueprint for redirection

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from os import makedirs
 from os.path import isdir, join
@@ -21,18 +21,13 @@ def create_app(test_config=None):
 
     Bootstrap(app)
 
-    @app.route('/')
-    def _(): 
-        is_logged_in = bool(session.get('user'))
-        return render_template('home.html', is_logged_in=is_logged_in)
-    
-
     with app.app_context():
         from .database_config import db
         db.init_app(app)
-    
 
     from .authentication.login import login_bp
+    from .home import bp as home
     app.register_blueprint(login_bp)
+    app.register_blueprint(home)
 
     return app
