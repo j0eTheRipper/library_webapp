c8d5ffc03ad3a9ae58cee7787e5652e67b622b7b
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-02-08 22:38:19 +0300

    reverse everything

--- tests/db.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, is_admin)
-VALUES ('admin', 'pbkdf2:sha256:260000$bjQfFlgJn9cxeStI$ccc213ba5e0e37a9d50315064c6c9db26e6014533a399e4bbb460e9b681bba54', true);
-
-INSERT INTO users (username, password, is_admin)
-VALUES ('user', 'pbkdf2:sha256:260000$bjQfFlgJn9cxeStI$ccc213ba5e0e37a9d50315064c6c9db26e6014533a399e4bbb460e9b681bba54', false);
