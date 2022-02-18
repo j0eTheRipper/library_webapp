83da935b3778c96bea933da5544724b209d6c6e8
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-02-18 15:40:09 +0300

    implemented remiders for borrows

--- app/index/index.py
+++ app/index/index.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, flash
+from ..database_config.models import Borrows
+from ..database_config.db import get_db, close_db
+from datetime import date
 
 
 bp = Blueprint('home', __name__, url_prefix='/')
@@ -9,6 +12,8 @@ def home():
     is_logged_in = bool(session.get('username'))
 
     if is_logged_in:
+        un_returned_books()
+
         if session.get('is_admin'):
             return render_template('index/admin.html')
         else:
@@ -16,3 +21,14 @@ def home():
     else:
         return render_template('index/guest.html')
 
+
+def un_returned_books():
+    db = get_db()
+    today = date.today()
+    all_borrows = db.query(Borrows).filter_by(borrower=session['username']).filter_by(is_returned=False)
+    expired = all_borrows.filter(today >= Borrows.date_returned).all()
+    un_returned = all_borrows.filter(today <= Borrows.date_returned).all()
+    if expired:
+        flash(f'You have {len(expired)} books that need to be returned! Please return', 'warning')
+    if un_returned:
+        flash(f'You have {len(un_returned)} unreturned books. Remember to return them on time!', 'success')
