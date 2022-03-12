bfa445f8b370f63209dd33c6fc196974cf26283c
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-03-12 14:04:03 +0300

    changes, untracked

--- app/index/index.py
+++ app/index/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, flash
+from flask import Blueprint, render_template, session, flash, Markup
 from ..database_config.models import Borrows
 from ..database_config.db import get_db, close_db
 from datetime import date
@@ -25,10 +25,12 @@ def home():
 def un_returned_books():
     db = get_db()
     today = date.today()
-    all_borrows = db.query(Borrows).filter_by(borrower=session['username']).filter_by(is_returned=False)
-    expired = all_borrows.filter(today >= Borrows.date_returned).all()
-    un_returned = all_borrows.filter(today <= Borrows.date_returned).all()
+    all_borrows = db.query(Borrows).filter_by(borrower=session['username']).filter_by(date_returned=None)
+    expired = all_borrows.filter(today >= Borrows.due_date).all()
     if expired:
-        flash(f'You have {len(expired)} books that need to be returned! Please return', 'warning')
-    if un_returned:
-        flash(f'You have {len(un_returned)} unreturned books. Remember to return them on time!', 'success')
+        message = Markup(
+            f'You have {len(expired)} books that need to be returned! Please <a href="/view_borrowed">return</a>'
+        )
+        flash(message, 'warning')
+
+    close_db()
