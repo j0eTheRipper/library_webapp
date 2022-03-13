03a4cd80cc57d079a9931f98bf1dafe36a6d5429
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-03-13 21:59:39 +0300

    added session controles test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,3 @@
-# from app.database_config.db import get_db, close_db
 from os.path import isfile
 from os import remove
 
@@ -12,3 +11,15 @@ def test_init_db_command(runner):
     result = runner.invoke(args=['init-db'])
     assert 'created successfully' in result.output
     assert isfile(db)
+
+
+def test_get_close_db(app):
+    from app.database_config.db import get_db, close_db
+    from flask import g
+
+    with app.app_context():
+        db = get_db()
+
+        assert g.db == db
+        close_db()
+        assert 'db' not in g
