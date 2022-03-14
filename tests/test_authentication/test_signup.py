d5a0af1af9bc8630598daed4141815e6bcb2d136
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-03-14 20:18:54 +0300

    Corrected authentication tests

--- tests/test_authentication/test_signup.py
+++ tests/test_authentication/test_signup.py
@@ -1,17 +1,22 @@
 URL = 'http://localhost/signup/'
 
 
-def test_signup(client, authenticate):
+def test_signup(app, client, authenticate):
     assert client.get(URL).status_code == 200
 
     response = authenticate.signup('new_user', 'password', 'password')
     assert response.headers['Location'] == 'http://localhost/'
 
-    test_registered_user(authenticate, client)
+    with app.app_context():
+        from app.database_config.db import get_db
+        from app.database_config.models import Users
+
+        db = get_db()
+        assert db.query(Users).filter_by(username='new_user').first() is not None
 
 
-def test_registered_user(authenticate, client):
-    response = authenticate.signup('new_user', 'password', 'password')
+def test_registered_user(app, authenticate, client):
+    response = authenticate.signup('admin', 'password', 'password')
     assert response.headers['Location'] == URL
 
     response = client.get(URL)
@@ -23,4 +28,4 @@ def test_non_matching_passwords(authenticate, client):
     assert response.headers['Location'] == URL
 
     response = client.get(URL)
-    assert b"The passwords don't match" in response.data
+    assert b"Passwords do not match!" in response.data
