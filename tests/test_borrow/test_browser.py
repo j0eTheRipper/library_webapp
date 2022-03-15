127958da60413238ecb8533e42403a6d39086a25
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-03-15 16:58:57 +0300

    writing tests for browsing books

--- tests/test_borrow/test_browser.py
+++ tests/test_borrow/test_browser.py
@@ -7,7 +7,20 @@ def test_invalid_access(client):
     assert response.headers['Location'] == 'http://localhost/login/'
 
 
-def test_valid_access(client, authenticate):
+def test_valid_access(app, client, authenticate):
     authenticate.login('admin', 'admin')
     response = client.get(URL)
     assert response.status_code == 200
+
+    with app.app_context():
+        from app.database_config.db import get_db
+        from app.database_config.models import Books
+
+        db = get_db()
+        all_books = db.query(Books).all()
+
+        for book in all_books:
+            assert bytes(book.title, encoding='UTF8') in response.data
+            assert bytes(book.author, encoding='UTF8') in response.data
+            assert bytes(book.subject, encoding='UTF8') in response.data
+            assert bytes(str(book.count), encoding='UTF8') in response.data
