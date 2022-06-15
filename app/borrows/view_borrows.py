6b99f7023aba4a18a523ef986079cbbbe825b075
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-06-15 22:50:15 +0300

    finished returned books history functionality

--- app/borrows/view_borrows.py
+++ app/borrows/view_borrows.py
@@ -2,26 +2,27 @@ from flask import Blueprint, render_template, request, session, url_for
 from datetime import date
 from ..database_config.db import get_db
 from ..database_config.models import Borrows
+from ..shared_functions import login_required
 
 
 bp = Blueprint('borrows', __name__, url_prefix='/borrows')
 
 
-@bp.route('/history')
-def view_borrows():
+@bp.route('/history_returned')
+@login_required
+def view_returned_borrows():
     db = get_db()
-    borrow_state_filter = request.args.get('filter')
-    borrows = db.query(Borrows)
+    borrows = db.query(Borrows).filter(Borrows.date_returned)
 
-    if borrow_state_filter == 'returned':
-        borrows = borrows.filter(Borrows.date_returned)
-        print(len(borrows.all()))
-    elif borrow_state_filter == 'unreturned':
-        borrows = borrows.filter_by(date_returned=None)
+    filters = request.args.get('filter')
+    if filters == 'overdue':
+        borrows = borrows.filter(Borrows.date_returned > Borrows.due_date)
+    elif filters == 'on_time':
+        borrows = borrows.filter(Borrows.date_returned <= Borrows.due_date)
 
-    if session['is_admin']:
+    if session.get('is_admin'):
         borrows = borrows.all()
     else:
-        borrows = borrows.filter_by(borrower=session['username']).all()
+        borrows = borrows.filter_by(borrower=session.get('username')).all()
 
-    return render_template('borrows/borrow_history.html', borrows=borrows, today=date.today())
+    return render_template('view_borrows/view_borrows.html', borrows=borrows, today=date.today())
