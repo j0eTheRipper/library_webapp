bd13d01e12d5c4d0404117f40ef601175bceb687
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-07-19 18:11:30 +0300

    testing in progress

--- app/index/index.py
+++ app/index/index.py
@@ -10,32 +10,35 @@ bp = Blueprint('home', __name__, url_prefix='/')
 @bp.route('/')
 def home():
     is_logged_in = bool(session.get('username'))
+    unreturned_books = 0
 
     if is_logged_in:
         if session.get('is_admin'):
-            unreturned_books = un_returned_books(True)
-            return render_template('index/admin.html', unreturned_books=len(unreturned_books))
+            unreturned_books = get_unreturned_books(True)
+            page_to_render = 'index/user.html'
         else:
-            unreturned_books = un_returned_books(False)
-            return render_template('index/user.html', unreturned_books=len(unreturned_books))
+            unreturned_books = get_unreturned_books(False)
+            page_to_render = 'index/admin.html'
     else:
-        return render_template('index/guest.html')
+        page_to_render = 'index/guest.html'
+
+    return render_template(page_to_render, unreturned_books=len(unreturned_books))
 
 
-def un_returned_books(is_admin):
+def get_unreturned_books(is_admin):
     db = get_db()
     today = date.today()
+    unreturned = db.query(Borrows).filter_by(date_returned=None)
 
-    if is_admin:
-        unreturned = db.query(Borrows).filter_by(date_returned=None)
-    else:
-        unreturned = db.query(Borrows).filter_by(borrower=session['username']).filter_by(date_returned=None)
+    if not is_admin:
+        unreturned = unreturned.filter_by(borrower=session['username'])
 
     expired = unreturned.filter(today >= Borrows.due_date).all()
     unreturned = unreturned.all()
     if expired:
         message = Markup(
-            f'You have {len(expired)} books that need to be returned! Please <a href="/view_borrowed">return</a>'
+            f'You have {len(expired)} books that need to be returned! Please <a '
+            'href="/borrows/history_unreturned">return</a> '
         )
         flash(message, 'warning')
 
