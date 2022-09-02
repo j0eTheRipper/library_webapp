c246074c471a83ac0fdd828c68222d31f6c46848
j0eTheRipper <j0eTheRipper0010@gmail.com>
2022-09-02 21:50:46 +0200

    drying the tests

---
+++ tests/repeated_tests/repeated_request_tests.py
@@ -0,0 +1,11 @@
+def unauthorized_access(client, url):
+    pre_login_response = client.get(url)
+    assert pre_login_response.status_code == 401
+    assert pre_login_response.headers['Location'] == 'http://localhost/login/'
+
+
+def request_user_page(client, authenticate, url, username, passwd):
+    authenticate.login(username, passwd)
+    response = client.get(url)
+    assert response.status_code == 200
+    return response
